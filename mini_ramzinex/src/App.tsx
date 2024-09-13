656ea2da23a50dc35d942117de9ad90ee35e939a
blueocean979 <sa.ocean97@gmail.com>
2024-09-13 13:20:21 +0330

    first commit

---
+++ mini_ramzinex/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import { Link } from 'react-router-dom'
+
+function App() {
+
+  return (
+      <Link to="/marketslist" >Click to see list of markets</Link>    
+  )
+}
+
+export default App
