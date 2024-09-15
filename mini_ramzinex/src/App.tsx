552a228dea1e8b2289423debd83893d42033a1a8
blueocean979 <sa.ocean97@gmail.com>
2024-09-15 17:45:45 +0330

    root Link style

--- mini_ramzinex/src/App.tsx
+++ mini_ramzinex/src/App.tsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 
 function App() {
   return (
+    <div className='linkWrapper'>
       <Link to="/marketslist" >Click to see list of markets</Link>
+    </div>
   )
 }
 
